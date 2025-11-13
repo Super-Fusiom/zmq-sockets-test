b44891ea77e78aea1551f251a4cadbac22511b5c
Paul Singh <80376466+Super-Fusiom@users.noreply.github.com>
2025-11-13 20:10:50 +1300

    Added 0mq and socket example code.

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server...")
+socket = context.socket(zmq.REQ)
+socket.connect("udp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(10):
+    print(f"Sending request {request} ...")
+    socket.send_string("Yes")
+
+    #  Get the reply.
+    message = socket.recv()
+    print(f"Received reply {request} [ {message} ]")
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import zmq
+import zmq.asyncio
+
+
 def main():
-    print("Hello from sockets-test!")
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*.5555")
+
+    while True:
+        message = socket.recv()
+
+        print(f"Received: {message}")
+        socket.send_string("Working?")
 
 
 if __name__ == "__main__":
